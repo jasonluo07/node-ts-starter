0fc78fff85ce8c5a3eb73f78dfa29f30ba80eb7f
Jason <yulaie1012@gmail.com>
2023-08-16 17:55:54 +0800

    dev

--- scripts/generateFakeData.ts
+++ scripts/generateFakeData.ts
@@ -7,7 +7,7 @@ import { SALT_ROUNDS } from '../src/constants';
 const DATA_DIRECTORY_PATH = path.resolve(process.cwd(), 'data');
 const PRODUCTS_NUM_RECORDS = 100;
 const USERS_NUM_RECORDS = 100;
-const BASE_PRICE_RANGE = { min: 100, max: 10000 };
+const PRICE_RANGE = { min: 100, max: 10000 };
 const PASSWORD = 'Password1';
 
 function createDataDir() {
@@ -26,14 +26,14 @@ function createCsvFile(fileName: string, headers: string, rows: string[]) {
 }
 
 function generateProducts() {
-  const headers = 'name,description,price\n';
+  const headers = 'name,description,original_price,discount_price\n';
   const rows: string[] = [];
 
   for (let i = 1; i <= PRODUCTS_NUM_RECORDS; i++) {
-    const basePrice =
-      Math.floor(Math.random() * (BASE_PRICE_RANGE.max - BASE_PRICE_RANGE.min + 1)) + BASE_PRICE_RANGE.min;
-    const price = Math.random() < 0.5 ? basePrice : basePrice + 0.5;
-    rows.push(`Product ${i},Description for Product ${i},${price}\n`);
+    const originalPrice = Math.floor(Math.random() * (PRICE_RANGE.max - PRICE_RANGE.min + 1)) + PRICE_RANGE.min;
+    const discountRate = (Math.floor(Math.random() * 9) + 2) / 10; // 0.2 ~ 1.0
+    const discountPrice = Math.floor(originalPrice * discountRate);
+    rows.push(`Product ${i},Description for Product ${i},${originalPrice},${discountPrice}\n`);
   }
 
   createCsvFile('products.csv', headers, rows);
